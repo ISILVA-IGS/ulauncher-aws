b99fc131b6ce58ebed7c9d318afc1c116eb912b7
igor <igor.silva@ewally.com.br>
2022-08-18 13:19:01 -0300

    test

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 
+
 class GnomeSessionExtension(Extension):
     def __init__(self):
         super(GnomeSessionExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
+
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
         options = [
-                        'ec2', 'ecs', 'rds', 's3', 'elasticbeanstalk', 'elasticache', 'cloudwatch', 'cloudformation', 'vpc', 'iam', 'ecr', 'eks', 'lambda', 'dynamodb',
-                        'managementconsole', 'management', 'console',
-                        'support', 'ticket', 'helpdesk', 'help',
-                        'billing', 'budget', 'costs',
-                        'pricingcalculator', 'pricing', 'price', 'prices', 'calculate', 'calculator',
-                        'compare', 'instancecomparison', 'comparison',
-                        'route53', 'dns', 'sqs', 'sns', 'ses', 'elasticsearch', 'kms', 'cloudfront', 'api', 'gateway',
-                        'cloudtrail', 'secret', 'systemsmanager'
-                  ]
+            'ec2', 'ecs', 'rds', 's3', 'elasticbeanstalk', 'elasticache', 'cloudwatch', 'cloudformation', 'vpc', 'iam', 'ecr', 'eks', 'lambda', 'dynamodb',
+            'managementconsole', 'management', 'console',
+            'support', 'ticket', 'helpdesk', 'help',
+            'billing', 'budget', 'costs',
+            'pricingcalculator', 'pricing', 'price', 'prices', 'calculate', 'calculator',
+            'compare', 'instancecomparison', 'comparison',
+            'route53', 'dns', 'sqs', 'sns', 'ses', 'elasticsearch', 'kms', 'cloudfront', 'api', 'gateway',
+            'cloudtrail', 'secret', 'systemsmanager'
+        ]
         my_list = event.query.split(" ")
         if len(my_list) == 1:
             items.append(get_ec2_item())
@@ -129,43 +131,55 @@ class KeywordQueryEventListener(EventListener):
                         items.append(get_systemsmanager_item())
             return RenderResultListAction(items)
 
+
 def get_api_gateway_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS API Gateway',
                                description='AWS API Gateway',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_elasticsearch_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Elasticsearch',
                                description='AWS Elasticsearch Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
+
 def get_kms_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS KMS',
                                description='AWS Key Management Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
+
 def get_cloudfront_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Cloudfront',
                                description='AWS CloudFront Manager',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
+
 def get_ec2_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS EC2',
                                description='AWS Elastic Compute Cloud',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
+
 def get_ecs_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS ECS',
                                description='EC2 Container Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_rds_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS RDS',
                                description='AWS Relational Database Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_s3_item():
     return ExtensionResultItem(icon='images/icon.png',
@@ -173,83 +187,97 @@ def get_s3_item():
                                description='AWS Simple Storage Service',
                                on_enter=OpenUrlAction("https://s3.console.aws.amazon.com/s3"))
 
+
 def get_elasticbeanstalk_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS ElasticBeanstalk',
                                description='AWS ElasticBeanstalk Application Environment',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_elasticache_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS ElastiCache',
                                description='AWS ElastiCache (Redis, Memcached, etc.)',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_cloudwatch_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS CloudWatch',
                                description='AWS CloudWatch Metrics and Monitoring',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_cloudformation_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS CloudFormation',
                                description='AWS Cloud Formation Cosole',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_vpc_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS VPC',
                                description='AWS Virtual Private Cloud',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_iam_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS IAM',
                                description='AWS Identity & Access Management',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_ecr_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS ECR',
                                description='AWS Elastic Container Registry',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_eks_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS EKS',
                                description='AWS Kubernetes Management Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_lambda_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Lambda',
                                description='AWS Lambda Serverless Computing',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_dynamodb_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS DynamoDB',
                                description='AWS DynamoDB NoSQL Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_managementconsole_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Management Console',
                                description='Manage all your AWS infrastructure',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_support_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Support Console',
                                description='Access AWS customer and business support ticketing system',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_billing_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Billing Dashboard',
                                description='AWS Billing & Cost Management Center. Manage Billing, Budgets, Cost Explorer and Reports ',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_pricingcalculator():
     return ExtensionResultItem(icon='images/icon.png',
@@ -257,53 +285,62 @@ def get_pricingcalculator():
                                description='AWS Pricing Calculator',
                                on_enter=OpenUrlAction("https://calculator.s3.amazonaws.com/index.html"))
 
+
 def get_compare():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Instance Comparision',
                                description='EC2Instances.info Easy Amazon EC2 Instance Comparison',
                                on_enter=OpenUrlAction("https://www.ec2instances.info"))
 
+
 def get_route53_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Route 53',
                                description='AWS Route 53 Domain & DNS Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_sqs_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Simple Queue Service',
                                description='AWS SQS Managed Message Queues',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_sns_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Simple Notification Service',
                                description='AWS SNS managed message topics for Pub/Sub',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk/v3"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk/v3"))
+
 
 def get_ses_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Simple Email Service',
                                description='AWS SES Email Sending and Receiving Service',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_secret_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Secrets Manager',
                                description='AWS Secrets Manager',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_cloudtrail_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS CloudTrail',
                                description='AWS CloudTrail',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 def get_systemsmanager_item():
     return ExtensionResultItem(icon='images/icon.png',
                                name='AWS Systems Manager',
                                description='AWS Systems Manager',
-                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=ZAqh1Q2bpCk-manager"))
+                               on_enter=OpenUrlAction("https://www.youtube.com/watch?v=4E8eOUEtChk"))
+
 
 if __name__ == '__main__':
     GnomeSessionExtension().run()
